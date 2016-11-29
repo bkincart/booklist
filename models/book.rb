3c5b60e66757992fc165c00adfd0fbab212d22a1
Ezra Skolnik <ezraskolnik@gmail.com>
2016-11-29 14:51:34 -0500

    simpler model

--- models/book.rb
+++ models/book.rb
@@ -3,11 +3,11 @@ require "csv"
 class Book
   attr_reader :title, :author, :rating, :url
 
-  def initialize(attributes = {})
-    @title = attributes["title"]
-    @author = attributes["author"]
-    @rating = attributes["rating"]
-    @url = attributes["url"]
+  def initialize(title, author, rating, url)
+    @title = title
+    @author = author
+    @rating = rating
+    @url = url
   end
 
   def save
@@ -16,34 +16,32 @@ class Book
     end
   end
 
-  class << self
-    def all
-      result = []
-      CSV.foreach(csv_file, csv_options) do |row|
-        result << Book.new(row.to_hash)
-      end
-      result
+  def self.all
+    result = []
+    CSV.foreach(csv_file, csv_options) do |row|
+      result << Book.new(row.to_hash)
     end
+    result
+  end
 
-    def csv_file
-      if ENV["RACK_ENV"] == "test"
-        return File.join(File.dirname(__FILE__), "..", "data", "books_test.csv")
-      else
-        return File.join(File.dirname(__FILE__), "..", "data", "books.csv")
-      end
+  def self.csv_file
+    if ENV["RACK_ENV"] == "test"
+      return File.join(File.dirname(__FILE__), "..", "data", "books_test.csv")
+    else
+      return File.join(File.dirname(__FILE__), "..", "data", "books.csv")
     end
+  end
 
-    def csv_options
-      {
-        col_sep: ";",
-        headers: true
-      }
-    end
+  def self.csv_options
+    {
+      col_sep: ";",
+      headers: true
+    }
+  end
 
-    def reset_csv
-      CSV.open(Book.csv_file, "wb", Book.csv_options) do |csv|
-        csv << ["title", "author", "rating", "url"]
-      end
+  def self.reset_csv
+    CSV.open(Book.csv_file, "wb", Book.csv_options) do |csv|
+      csv << ["title", "author", "rating", "url"]
     end
   end
 end
